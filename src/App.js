d7c1fa07db14471c796bf106e7e790640cb55424
LuisChavezDelgado <17460348@colima.tecnm.mx>
2022-11-10 10:30:35 -0600

    Setup Initial And Home Screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import './App.css';
-import {Button} from 'react-bootstrap';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Login } from "./components/Login";
+import { Register } from "./components/Register";
+import { Home } from "./components/Home";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+
+import { AuthProvider } from "./context/authContext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-        <Button>Aceptar</Button>
-      </header>
+    <div className="bg-slate-300 text-black h-screen flex text-white">
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </AuthProvider>
     </div>
   );
 }
